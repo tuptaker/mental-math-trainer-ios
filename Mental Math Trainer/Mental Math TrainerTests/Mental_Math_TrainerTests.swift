60fa7d3b9433e34ef9e1e15a3c31fcf356fc5bfd
Fabricio Machado <fabrizio.machado@tup-take.com>
2017-05-26 07:45:03 -0400

    initial project

---
+++ Mental Math Trainer/Mental Math TrainerTests/Mental_Math_TrainerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mental_Math_TrainerTests.swift
+//  Mental Math TrainerTests
+//
+//  Created by Fabricio Machado on 5/26/17.
+//  Copyright © 2017 Fabricio Machado. All rights reserved.
+//
+
+import XCTest
+@testable import Mental_Math_Trainer
+
+class Mental_Math_TrainerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
